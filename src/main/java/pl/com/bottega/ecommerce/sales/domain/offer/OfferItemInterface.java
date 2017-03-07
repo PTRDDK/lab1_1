04a00820e402ff490831cc94f40352e537b06845
Piotr Dudek <194936@edu.p.lodz.pl>
2017-03-07 08:54:38 -0800

    Stworzenie interfejsu dla OfferItem

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferItemInterface.java
@@ -0,0 +1,7 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+public interface OfferItemInterface {
+	int hashCode();
+	boolean equals(Object obj);
+	boolean sameAs(OfferItem other, double delta);
+}
